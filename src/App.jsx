f5305703e7c4e6dd822738f8bd231b2de0c0adef
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-06-04 21:57:39 +0530

    User Authentication Implementation

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { firebaseApp } from './firebase';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { Link } from 'react-router-dom';
+
+const auth = getAuth(firebaseApp);
+
+function App() {
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUser(user);
+			} else {
+				setUser(null);
+			}
+		});
+	}, []);
+
+	return (
+		<div className='h-screen pb-16 bg-gradient-to-r from-orange-600 to-pink-600 text-white'>
+			<div className='text-3xl sm:text-4xl md:text-5xl py-10 font-bold text-center text-white drop-shadow-lg'>
+				Welcome to Firebase Auth
+			</div>
+
+			{!user ? (
+				<div className='my-10 py-4 flex justify-center flex-wrap gap-4'>
+					<Link
+						to='/login'
+						className='w-[200px] px-2 py-2 rounded-md bg-blue-700 text-white text-center hover:bg-blue-800 shadow-md'
+					>
+						Go to Login
+					</Link>
+					<Link
+						to='/register'
+						className='w-[200px] px-2 py-2 rounded-md bg-orange-500 text-white text-center hover:bg-orange-700 shadow-md'
+					>
+						Go to Registration
+					</Link>
+				</div>
+			) : (
+				<div className='w-full max-w-[600px] mx-auto my-10 py-4 rounded-md flex flex-col justify-center items-center flex-wrap gap-4 shadow-xl bg-transparent/5 backdrop-blur-2xl drop-shadow-lg'>
+					<h2 className='text-2xl sm:text-3xl md:text-4xl font-medium my-2'>
+						Hey {user.displayName}
+					</h2>
+					<h3 className='text-lg sm:text-xl md:text-2xl'>You are logged in!</h3>
+					<button
+						onClick={() => signOut(auth)}
+						className='w-[180px] my-4 px-4 py-2 rounded-md bg-blue-700 text-white hover:bg-blue-800 shadow-md'
+					>
+						Logout
+					</button>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default App;
---
+++ src/context/Firebase.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext } from 'react';
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	updateProfile,
+} from 'firebase/auth';
+import { firebaseApp } from '../firebase.js';
+
+const firebaseAuth = getAuth(firebaseApp);
+
+const FirebaseContext = createContext(null);
+
+export const useFirebase = () => useContext(FirebaseContext);
+
+export const FirebaseProvider = (props) => {
+	const registerUserWithEmailAndPassword = (email, password) => {
+		return createUserWithEmailAndPassword(firebaseAuth, email, password);
+	};
+
+	const loginWithEmailAndPassword = (email, password) => {
+		return signInWithEmailAndPassword(firebaseAuth, email, password);
+	};
+
+	const updateUserProfile = (name) => {
+		return updateProfile(firebaseAuth.currentUser, {
+			displayName: name,
+		});
+	};
+
+	return (
+		<FirebaseContext.Provider
+			value={{
+				registerUserWithEmailAndPassword,
+				loginWithEmailAndPassword,
+				updateUserProfile,
+			}}
+		>
+			{props.children}
+		</FirebaseContext.Provider>
+	);
+};
